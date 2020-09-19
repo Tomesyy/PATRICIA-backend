3aee1d09a845bf76b3d704fb477f8429c041c82f
Tomesyy <adetomiwaadesanya01@gmail.com>
2020-09-19 13:51:38 +0100

    setup users table model

--- database/migrations/2020_09_19_124609_create_users_table.php
+++ database/migrations/2020_09_19_124609_create_users_table.php
@@ -14,7 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
             $table->timestamps();
         });
     }
