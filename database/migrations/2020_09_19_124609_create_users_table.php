70e0983d322b675e448494bf9a21b4b92a937963
Tomesyy <adetomiwaadesanya01@gmail.com>
2020-09-19 13:53:43 +0100

    set emails field in model to be unique

--- database/migrations/2020_09_19_124609_create_users_table.php
+++ database/migrations/2020_09_19_124609_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique()->notNullable();
             $table->string('password');
             $table->timestamps();
         });
